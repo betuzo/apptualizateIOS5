3cea02c62e7888925693a7c09f18afecfe5d4ff7
betuzo <betotsol@gmail.com>
2012-03-21 23:55:37 -0600

    Cell a la medida para los productos (iPhone)

--- apptualizateIOS5/FeaturedViewController.h
+++ apptualizateIOS5/FeaturedViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FeaturedViewController : UIViewController
+@interface FeaturedViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
 {
 }
 
