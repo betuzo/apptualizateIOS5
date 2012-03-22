0397617806546a25801fd63316939e88b051171b
betuzo <betotsol@gmail.com>
2012-03-21 19:15:33 -0600

    Home para iPad

---
+++ apptualizateIOS5/HomeViewControlleriPad.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewControlleriPad.h
+//  apptualizateIOS5
+//
+//  Created by Roberto Salvador Olguín Lozano on 21/03/12.
+//  Copyright (c) 2012 Valle del Bit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewControlleriPad : UIViewController
+
+@end
