eebdcd861673721109a936e996060b3b82846af9
betuzo <betotsol@gmail.com>
2012-03-22 16:59:11 -0600

    Image TabController

--- apptualizateIOS5/FeaturedParentViewController.m
+++ apptualizateIOS5/FeaturedParentViewController.m
@@ -39,7 +39,7 @@
     
     [controller setFeaturedProducts:[[NSArray alloc] initWithObjects:
                                     [[NSArray alloc] initWithObjects:@"iPod touch", @"Tu música en tu dedo", @"mini-IpodT", nil],
-                                    [[NSArray alloc] initWithObjects:@"iPad 2", @"Tu mejor aliado", @"mini-iPad", nil], 
+                                    [[NSArray alloc] initWithObjects:@"iPad 2", @"Tu mejor aliado", @"iPadOK", nil], 
                                     [[NSArray alloc] initWithObjects:@"iPod nano", @"Un pequeño gigante", @"mini-iPodN", nil],
                                     [[NSArray alloc] initWithObjects:@"iPhone 4", @"Conectate al mundo", @"mini-iphone", nil], 
                                     [[NSArray alloc] initWithObjects:@"MacBook Pro", @"Tu mejor aliado", @"mini-iphone-negro", nil], 
--- apptualizateIOS5/HomeViewController.m
+++ apptualizateIOS5/HomeViewController.m
@@ -42,12 +42,35 @@
 	
 	
 	UIViewController *viewController1 = [[FeaturedParentViewController alloc] initWithNibName:@"FeaturedParentViewController" bundle:nil];
+    UITabBarItem *tab1 = [[UITabBarItem alloc] initWithTitle:@""
+                                                       image:[UIImage imageNamed:@"destacados-on"] tag:1];
+    [viewController1 setTabBarItem:tab1];  
+    
 	UIViewController *viewController2 = [[ProductsParentViewController alloc] initWithNibName:@"ProductsParentViewController" bundle:nil];
+    UITabBarItem *tab2 = [[UITabBarItem alloc] initWithTitle:@""
+                                                       image:[UIImage imageNamed:@"productos-on"] tag:2];
+    [viewController2 setTabBarItem:tab2];      
+    
 	UIViewController *viewController3 = [[StoresParentViewController alloc] initWithNibName:@"StoresParentViewController" bundle:nil];
-    UIViewController *viewController4 = [[LoyaltyViewController alloc] initWithNibName:@"LoyaltyViewController" bundle:nil];
-    UIViewController *viewController5 = [[ShopViewController alloc] initWithNibName:@"ShopViewController" bundle:nil];
+    UITabBarItem *tab3 = [[UITabBarItem alloc] initWithTitle:@""
+                                                       image:[UIImage imageNamed:@"tiendas-on"] tag:3];
+    [viewController3 setTabBarItem:tab3];  
+    
+    UIViewController *viewController4 = [[ShopViewController alloc] initWithNibName:@"ShopViewController" bundle:nil];
+    UITabBarItem *tab4 = [[UITabBarItem alloc] initWithTitle:@""
+                        image:[UIImage imageNamed:@"carrito-on"] tag:4];
+    [viewController4 setTabBarItem:tab4];    
+
+    
+    UIViewController *viewController5 = [[LoyaltyViewController alloc] initWithNibName:@"LoyaltyViewController" bundle:nil];
+    UITabBarItem *tab5 = [[UITabBarItem alloc] initWithTitle:@""
+                                                       image:[UIImage imageNamed:@"fan-on"] tag:5];
+    [viewController5 setTabBarItem:tab5];    
+
+
 	
 	_homeTabBarController = [[UITabBarController alloc] init];
+    
 	_homeTabBarController.viewControllers = [NSArray arrayWithObjects:viewController1, viewController2,viewController3,viewController4,viewController5,nil];
  	[_homeTabBarController.view setFrame:self.view.bounds];
 	
