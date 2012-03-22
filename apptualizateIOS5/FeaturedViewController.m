add3cf7d8b9fbd9c4bb36412b7412ce420ce8cc3
Cristhian Velazco <cristhianvs@yahoo.com>
2012-03-22 12:10:52 -0600

    Iphone actualizado

--- apptualizateIOS5/FeaturedViewController.m
+++ apptualizateIOS5/FeaturedViewController.m
@@ -13,6 +13,7 @@
 @implementation FeaturedViewController
 
 @synthesize featuredProducts = _featuredProducts;
+@synthesize myFeaturedImage;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
