7f7b831576fbadb2eb0e36ba4cb5e4ecc6069221
misaelpc <misaelpcgm@gmail.com>
2012-03-23 10:01:20 -0600

    pin added

---
+++ apptualizateIOS5/MacStorePinAnotation.h
@@ -0,0 +1,13 @@
+//
+//  MacStorePinAnotation.h
+//  apptualizateIOS5
+//
+//  Created by Misael Pérez Chamorro on 3/23/12.
+//  Copyright (c) 2012 Valle del Bit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MacStorePinAnotation : NSObject
+
+@end
