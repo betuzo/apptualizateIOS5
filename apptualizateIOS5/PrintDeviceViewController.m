f21db3e5f6edd808b2770a383f8df3e5fd271a24
Cristhian Velazco <cristhianvs@yahoo.com>
2012-03-22 23:51:37 -0600

    New features

---
+++ apptualizateIOS5/PrintDeviceViewController.m
@@ -0,0 +1,44 @@
+//
+//  PrintDeviceViewController.m
+//  apptualizateIOS5
+//
+//  Created by Cristhian Velazco Solano on 22/03/12.
+//  Copyright (c) 2012 Valle del Bit. All rights reserved.
+//
+
+#import "PrintDeviceViewController.h"
+
+@interface PrintDeviceViewController ()
+
+@end
+
+@implementation PrintDeviceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
